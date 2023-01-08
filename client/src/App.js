f7356baf3d09b517676937255d0e1ea0a8646f22
TingCythia <91254839+TingCythia@users.noreply.github.com>
2023-01-08 15:49:37 +0100

    change the file location

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Confirmation from "./scenes/checkout/Confirmation";
 import Home from "./scenes/home/Home";
 import ItemDetials from "./scenes/itemDetails/itemDetails";
 import Navbar from "./scenes/global/Navbar";
-
+import ShoppingList from "./components/ShoppingList";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -27,6 +27,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="item/:itemId" element={<ItemDetials />} />
+          <Route path="item" element={<ShoppingList/>} />
           <Route path="checkout" element={<Checkout />} />
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,9 +1,9 @@
-import ShoppingList from "../../components/ShoppingList";
+
 const Home = () => {
 
   return (
     <div className="home">
-    <ShoppingList />
+   
     </div>
   );
 };
